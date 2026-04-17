2532e6f177c5ac19243cd5c7f185dc40c40257bf
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-04-17 09:09:07 +0530

    added new statuscode

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,13 +7,18 @@ export interface ImageType {
   [key: string]: any;
 }
 
-
 export const statusCodes = {
-    SUCCESS: 200,
-    CREATED: 201,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    FORBIDDEN: 403,
-    NOT_FOUND: 404,
-    INTERNAL_SERVER_ERROR: 500,
-}
+  SUCCESS: 200,
+  CREATED: 201,
+
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+
+  CONFLICT: 409,
+  TO_MANY_REQUESTS: 429,
+
+  
+  INTERNAL_SERVER_ERROR: 500,
+};
