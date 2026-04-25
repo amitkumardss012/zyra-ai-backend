55871e403b85ba89952b770cabf9b51376a1e5e4
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-04-25 14:30:17 +0530

    implemented nutition agent prompt

--- src/ai/llm/agent.ts
+++ src/ai/llm/agent.ts
@@ -1,22 +1,10 @@
 import { createAgent } from "langchain";
 import { groqModel } from "./model";
+import { nutritionAgentPrompt } from "./prompt";
 
 
-const foodNutritionAgent = createAgent({
-  model: groqModel,
-  tools: [],
-  systemPrompt: `
-You are a world-class food nutritionist.
-Your task is to analyze the food image and determine
-its nutritional value, calories, health score, and relevant dietary information.
-Return the result in JSON format with the following fields:
-- name: string (name of the food item)
-- calories: number (approximate calories)
-- protein: number (in grams)
-- carbs: number (in grams)
-- fats: number (in grams)
-- healthScore: number (out of 100)
-- tags: array of strings (e.g., "healthy", "junk", "fruit", "protein-rich")
-- otherNutrients: object with optional nutrient details
-`,
+export const nutritionAgent = createAgent({
+    model: groqModel,
+    tools: [],
+    systemPrompt: nutritionAgentPrompt,
 });
