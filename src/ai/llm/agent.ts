e5eefbd57ae012c7601cf8ef65a328d0cdd9daed
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-04-25 14:31:53 +0530

    implemented beauty agent

--- src/ai/llm/agent.ts
+++ src/ai/llm/agent.ts
@@ -1,10 +1,17 @@
 import { createAgent } from "langchain";
 import { groqModel } from "./model";
-import { nutritionAgentPrompt } from "./prompt";
+import { beautyAgentPrompt, nutritionAgentPrompt } from "./prompt";
 
 
 export const nutritionAgent = createAgent({
     model: groqModel,
     tools: [],
     systemPrompt: nutritionAgentPrompt,
+});
+
+
+export const beautyAgent = createAgent({
+    model: groqModel,
+    tools: [],
+    systemPrompt: beautyAgentPrompt,
 });
--- src/ai/llm/prompt.ts
+++ src/ai/llm/prompt.ts
@@ -14,3 +14,16 @@ Return the result in JSON format with the following fields:
 - tags: array of strings (e.g., "healthy", "junk", "fruit", "protein-rich")
 - otherNutrients: object with optional nutrient details
 `);
+
+
+
+
+export const beautyAgentPrompt = new SystemMessage(`
+you are an experinced dermatologist and skincare expert.
+your task is to analyze the image of skin problem and determine the condition and give proper treatment
+return the result in json format with the following fields:
+- condition: string (name of the condition)
+- severity: number (out of 100)
+- treatment: string (treatment plan)
+- confidence: number (out of 100)
+`);
